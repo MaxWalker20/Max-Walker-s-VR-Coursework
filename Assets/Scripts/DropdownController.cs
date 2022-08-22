e45787a99510a16b4a4be44396a31fd23db397a0
MaxWalker20 <17007433@hope.ac.uk>
2022-08-22 08:55:57 +0100

    dropdown now working

--- Assets/Scripts/DropdownController.cs
+++ Assets/Scripts/DropdownController.cs
@@ -7,25 +7,26 @@ public class DropdownController : MonoBehaviour
 
     void Start()
     {
-
+        Debug.Log("dropdown started");
         //Fetch the Dropdown GameObject
         m_Dropdown = GetComponent<Dropdown>();
         //Add listener for when the value of the Dropdown changes, to take action
         m_Dropdown.onValueChanged.AddListener(delegate {
-
+          Debug.Log("dropdown change detected");
           DropdownValueChanged(m_Dropdown);
         });
     }
-
-    //Ouput the new value of the Dropdown into Text
+    
     void DropdownValueChanged(Dropdown change)
     {
+      Debug.Log("function called");
+      ratioDecider(m_Dropdown);
       Graph graph = new Graph();
       graph.ratioNumerator = 3f;
       graph.ratioDenominator = 5f;
     }
 
     void ratioDecider(Dropdown change){
-
+      Debug.Log(change.value);
     }
 }
